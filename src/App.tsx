5cb8fc6539cf502e2c6563d8e05290f6b09a88d3
Ajay Manikanta <ajay.vijay1123@gmail.com>
2023-05-18 10:33:10 +0530

    Remove dist from git ignore file

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
-import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <div>
@@ -17,17 +14,6 @@ function App() {
         </a>
       </div>
       <h1>Vite + React + Ajay</h1>
-      <div className='card'>
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className='read-the-docs'>
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
